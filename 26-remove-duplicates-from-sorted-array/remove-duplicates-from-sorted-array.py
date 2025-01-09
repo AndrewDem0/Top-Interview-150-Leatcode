4b068c94e1cb11e666dd3d12d672484ca02ec3a6
Andrew Demudenko <144722259+AndrewDem0@users.noreply.github.com>
2025-01-10 00:42:30 +0200

    Time: 0 ms (100.00%) | Memory: 18.9 MB (20.89%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,5 +1,11 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        counter = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[counter] = nums[i]
+                counter += 1
+        return counter
         """
         This code uses the two-pointer technique and checks by comparing the current element in the
         array with the previous index. "The current element is compared with the previous one to 
@@ -7,9 +13,3 @@ class Solution:
         starts from the element at index 1 to prevent an error caused by checking values outside
         the array.
         """
-        counter = 1
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i - 1]:
-                nums[counter] = nums[i]
-                counter += 1
-        return counter
