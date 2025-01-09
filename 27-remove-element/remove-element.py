25d4f285aeefe816420edf4021d806cfc7bfe27d
Andrew Demudenko <144722259+AndrewDem0@users.noreply.github.com>
2025-01-10 00:48:13 +0200

    Update remove-element.py
    add code description

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,5 +1,11 @@
 class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
+    def removeElement(self, nums: List[int], val: int) -> int: 
+        """ 
+        This task uses two pointers to traverse the array:
+        i is used to check the value of each element to satisfy the condition,
+        and counter is used to track the position where the element that satisfies the condition should be moved.
+        Since the task does not require preserving the elements that do not meet the condition, they can be replace
+        """
         counter = 0
         for i in range(len(nums)):
             if nums[i] != val:
