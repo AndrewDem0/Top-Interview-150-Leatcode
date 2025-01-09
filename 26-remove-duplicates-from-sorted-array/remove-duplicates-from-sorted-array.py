ab550da94eb5da059f1eb5d003f00cff63eca078
Andrew Demudenko <144722259+AndrewDem0@users.noreply.github.com>
2025-01-10 00:44:25 +0200

    Time: 3 ms (50.19%) | Memory: 19 MB (12.71%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -7,9 +7,9 @@ class Solution:
                 counter += 1
         return counter
 """
-        This code uses the two-pointer technique and checks by comparing the current element in the
-        array with the previous index. "The current element is compared with the previous one to 
-        find unique elements and avoid going out of bounds of the list. Additionally, the iteration
-        starts from the element at index 1 to prevent an error caused by checking values outside
-        the array.
-        """
+This code uses the two-pointer technique and checks by comparing the current element in the
+array with the previous index. "The current element is compared with the previous one to 
+find unique elements and avoid going out of bounds of the list. Additionally, the iteration
+starts from the element at index 1 to prevent an error caused by checking values outside
+the array.
+"""
