57fe7c6e14ca78864741217efa68bcc3db78fb71
Andrew Demudenko <144722259+AndrewDem0@users.noreply.github.com>
2025-01-10 20:11:34 +0200

    Time: 54 ms (50.20%) | Memory: 17.6 MB (41.39%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -1,5 +1,11 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        """
+        This code uses two pointers to save memory. The `i` pointer is used for traversing
+        and detecting duplicates, while `counter` constructs the resulting list and keeps
+        track of the number of valid elements. The search for duplicate elements starts
+        at index 2 because, in any case, the first two numbers will satisfy the conditions.
+        """
         counter = 2
         for i in range(2, len(nums)):
             if nums[i] != nums[counter - 2]:
