8dc76599ffe27e9f9de53bd0e67a7594124fcdbf
Andrew Demudenko <144722259+AndrewDem0@users.noreply.github.com>
2025-01-10 20:45:29 +0200

    Time: 7 ms (57.81%) | Memory: 19.4 MB (21.03%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,11 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        dict = {} #use dictionary to track how many times does each number appear
-        for i in nums:
-            if i not in dict:
-                dict[i] = 1
-            else:
-                dict[i] += 1
-        for key, times in dict.items():
-            if times >= len(nums) / 2:
-                return key
+        candidate = None
+        count = 0
+        
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += 1 if num == candidate else -1
+        
+        return candidate
