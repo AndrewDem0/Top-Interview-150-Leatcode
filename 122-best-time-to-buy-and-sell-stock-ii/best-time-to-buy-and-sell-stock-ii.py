aceb36d9dadab41a57d6819de86d30856c477ea0
Andrew Demudenko <144722259+AndrewDem0@users.noreply.github.com>
2025-01-13 12:33:19 +0200

    Time: 0 ms (100.00%) | Memory: 18.9 MB (23.77%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,8 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        """
+        сенс полягає в тому щоб продавати акції як тільки знайдеться варіант з прибутком 
+        використаний елементарний спосіб порівнняння найближчих значень масиву, далі відбувається
+        вирахування профіту і значення всих вдалих операціцй повертається.
+
+        Complexity: Time complexity: O(n), Space complexity: O(1)
+
+        посилання на роз'яснення: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/solutions/5816678/video-sell-a-stock-immediately
+        """
         profit = 0
+
         for i in range(1, len(prices)):
             if prices[i] > prices[i - 1]:
                 profit += prices[i] - prices[i - 1]
-                
+
         return profit
