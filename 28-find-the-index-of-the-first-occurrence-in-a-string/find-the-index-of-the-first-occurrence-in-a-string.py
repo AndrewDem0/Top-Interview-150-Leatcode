f6fe9615d6e465e90bd355d303ca303ef7ac43c3
Andrew Demudenko <144722259+AndrewDem0@users.noreply.github.com>
2025-01-17 15:14:56 +0200

    Time: 0 ms (100.00%) | Memory: 17.6 MB (42.95%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,6 +1,12 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        for i in range(len(haystack + needle) - len(needle)):
+        """
+        цей алгоритм проходить весь рядок haystack і шукає на проміжку довжиною
+        шуканого слова це слово, якщо не знаходить його то пересувається на 1 
+        індекс вперед поки не пройде весь рядок, оскільки не можна щоб при пошуку
+        слова індекс i +... виходив за рамки довжини основного рядка то підрахунок
+        """
+        for i in range(len(haystack)):
             if haystack[i : i + len(needle)] == needle:
                 return i
         return -1
