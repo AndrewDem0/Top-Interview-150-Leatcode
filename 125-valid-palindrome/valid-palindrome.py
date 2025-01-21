39f1678a48b89e9a92d3dd5ca49fd971403a400b
Andrew Demudenko <144722259+AndrewDem0@users.noreply.github.com>
2025-01-21 14:52:07 +0200

    Time: 8 ms (53.44%) | Memory: 17.8 MB (76.35%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,6 +1,35 @@
+"""
+в першому варіант спочатку створюється новий рядок вже очищений від посторонніх символів
+та записанийу нижньому регістрі після чого йде порівняння на полідром і видається відповідь
+
+Time complexity: O(n) Space complexity: O(n)
+
+другий алгоритм використовує два вказівники для преміщення по рядку і одночасно фільтрує
+його елементи, не створюючи нового рядка.
+
+Time complexity: O(n) Space complexity: O(1)
+"""
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         actual_s =''.join(filter(str.isalnum, s)).lower()
 
-        if actual_s != actual_s[::-1]: return False
+        if actual_s != actual_s[::-1]: 
+            return False
+        return True
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+        
+        while left < right:
+            if not s[left].isalnum():
+                left += 1
+            elif not s[right].isalnum():
+                right -= 1
+            elif s[left].lower() != s[right].lower():
+                return False
+            else:
+                left += 1
+                right -= 1
+                
         return True
