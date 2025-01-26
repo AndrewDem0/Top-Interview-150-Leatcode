d64ef9cf96f9916856394093ba06fe9f967aa1a2
Andrew Demudenko <144722259+AndrewDem0@users.noreply.github.com>
2025-01-26 23:57:31 +0200

    Time: 0 ms (100.00%) | Memory: 18.5 MB (42.93%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,5 +1,16 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        """
+        This method uses the two-pointer approach to find the result. If the sum of two numbers
+        is greater than the target, the right pointer moves to the left. If the sum is smaller,
+        the left pointer moves to the right. In case of the correct answer, the values 
+        are returned.
+
+        Link to the solution: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/solutions/5101915/video-using-two-pointers-to-solve-the-question-with-o-1-space
+
+        Time complexity: O(n) Space complexity: O(1)
+        """
+
         left = 0
         right = len(numbers) - 1
 
