315989e807b42f4f33c6a2bfc0235247382c0b91
Andrew Demudenko <144722259+AndrewDem0@users.noreply.github.com>
2025-01-27 20:50:43 +0200

    Time: 111 ms (21.90%) | Memory: 28.5 MB (47.99%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,5 +1,19 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        """
+        The algorithm uses two pointers: the left pointer starts at the beginning of 
+        the array, and the right pointer starts at the end of the array.
+        The built-in `min` function is used to find the smaller of the two columns to
+        determine the x-coordinate,
+        while the y-value is calculated as the difference between the pointers.
+        The built-in `max` function is then applied to store the largest area found so far.
+        The result is returned at the end. The pointers move by one position
+        based on the following rule:
+        the pointer corresponding to the smaller value moves.
+
+        Time complexity: O(n) Space complexity: O(1)
+        """
+
         left_p = 0
         right_p = len(height) - 1
         champ = 0
