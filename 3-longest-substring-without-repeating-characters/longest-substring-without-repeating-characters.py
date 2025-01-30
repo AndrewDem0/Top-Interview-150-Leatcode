f0f720bd1db8352b0947da32a24a5e72f04ee2c0
Andrew Demudenko <144722259+AndrewDem0@users.noreply.github.com>
2025-01-30 17:52:35 +0200

    Time: 12 ms (89.54%) | Memory: 17.7 MB (62.35%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,5 +1,19 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        """
+        This algorithm uses the sliding window technique. As the `right` pointer moves
+        through the array, it adds each encountered character to the `char_set`.
+
+        If, during an iteration, we encounter a character that is already in `char_set`,
+        we remove characters from the set that the `left` pointer is pointing to, moving `left`
+        forward until the duplicate character is removed.
+
+        When `right` encounters a new character, it adds it to `char_set` and updates
+        `max_len`, comparing it to the longest sequence found so far.
+
+        Time complexity: O(n) Space complexity: O(1)
+        """
+
         max_len = 0
         left = 0
         char_set = set()
@@ -9,8 +23,8 @@ class Solution:
             while s[right] in char_set:
                 char_set.remove(s[left])
                 left += 1
-            
+
             char_set.add(s[right])
-            max_len = max(max_len, right - left +1)
+            max_len = max(max_len, right - left + 1)
 
         return max_len
