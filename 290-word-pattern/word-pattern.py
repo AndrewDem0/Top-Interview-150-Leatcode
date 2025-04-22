781467dc8f5dad899e74fcce106615b605d72f87
Andrew Demudenko <144722259+AndrewDem0@users.noreply.github.com>
2025-04-22 23:45:21 +0300

    Update word-pattern.py

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -16,3 +16,23 @@ class Solution:
             if len(s_list) > 1:
                 index += 1
         return True
+
+def wordPattern(self, pattern: str, s: str) -> bool:
+    words = s.split()
+    if len(pattern) != len(words):
+        return False
+
+    char_to_word = {}
+    word_to_char = {}
+
+    for c, w in zip(pattern, words):
+        if c in char_to_word:
+            if char_to_word[c] != w:
+                return False
+        else:
+            if w in word_to_char:
+                return False
+            char_to_word[c] = w
+            word_to_char[w] = c
+
+    return True
