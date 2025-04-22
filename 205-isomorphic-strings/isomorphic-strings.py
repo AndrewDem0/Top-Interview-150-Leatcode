81824137477fea84975ab4ed7f3a1b6b36b7ffb3
Andrew Demudenko <144722259+AndrewDem0@users.noreply.github.com>
2025-04-22 19:19:30 +0300

    Time: 3 ms (95.64%) | Memory: 17.9 MB (80.49%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -2,3 +2,17 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         return len(set(s)) == len(set(t)) == len(set(zip(s,t))) 
 
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        char_m = {} 
+        for sc, tc in zip(s,t):
+            if sc in char_m:
+                if char_m[sc] != tc:
+                    return False
+            elif tc in char_m.values():
+                return False
+            
+            char_m[sc] = tc
+        
+        return True
+
