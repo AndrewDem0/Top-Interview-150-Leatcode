6faf5c566572717ac40b8c7eb0ad48e657be3455
Andrew Demudenko <144722259+AndrewDem0@users.noreply.github.com>
2025-04-23 20:33:32 +0300

    Time: 9 ms (76.77%) | Memory: 17.8 MB (64.33%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,20 @@
+class Solution:
+    def MakeHashMap(self, string, HashMap):
+        for char in string:
+            HashMap[char] = 1 + HashMap.get(char, 0)
+        return HashMap
+
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        Hash_s = {}
+        Hash_t = {}
+        self.MakeHashMap(s, Hash_s)
+        self.MakeHashMap(t, Hash_t)
+
+        for i in Hash_s.keys():
+            if i not in Hash_t.keys() or Hash_s[i] != Hash_t[i]:
+                return False
+        
+        return True
