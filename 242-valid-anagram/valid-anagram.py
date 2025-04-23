06f87e0a1f0b85c7ed43ecbe772ec935fe8f3f1e
Andrew Demudenko <144722259+AndrewDem0@users.noreply.github.com>
2025-04-23 20:43:40 +0300

    Time: 7 ms (89.31%) | Memory: 18 MB (49.66%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -8,17 +8,7 @@ class Solution:
         return HashMap
 
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-
-        Hash_s = self.MakeHashMap(t)
-        Hash_t = self.MakeHashMap(s)
-
-        for i in Hash_s.keys():
-            if i not in Hash_t.keys() or Hash_s[i] != Hash_t[i]:
-                return False
-        
-        return True
+        return self.make_hash_map(s) == self.make_hash_map(t)
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
