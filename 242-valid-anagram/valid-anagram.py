d3de7626c5e52b47633c60f36bd4c605407f59cd
Andrew Demudenko <144722259+AndrewDem0@users.noreply.github.com>
2025-04-23 20:42:00 +0300

    Time: 7 ms (89.31%) | Memory: 18.1 MB (33.36%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 class Solution:
     def MakeHashMap(self, string):
         HashMap = {}
@@ -17,3 +19,29 @@ class Solution:
                 return False
         
         return True
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        for letter in set(s):
+            if s.count(letter) != t.count(letter):
+                return False
+        return True
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
+
+def isAnagram(s, t):
+    if len(s) != len(t):
+        return False
+
+    count = {}
+    for c in s:
+        count[c] = count.get(c, 0) + 1
+    for c in t:
+        if c not in count or count[c] == 0:
+            return False
+        count[c] -= 1
+    return True
